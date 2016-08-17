e402f5a19a678b45137603a6f328acb45259e71d
Vongola <vongola12324@coder.tw>
2016-08-17 21:16:28 +0800

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,24 @@
 var elixir = require('laravel-elixir');
 
 /*
-   |--------------------------------------------------------------------------
-   | Elixir Asset Management
-   |--------------------------------------------------------------------------
-   |
-   | Elixir provides a clean, fluent API for defining some basic Gulp tasks
-   | for your Laravel application. By default, we are compiling the Sass
-   | file for our application, as well as publishing vendor resources.
-   |
-   */
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
 var gulp = require('gulp');
-var build = require('./semantic/tasks/build')
-var exec = require('child_process').exec;
-var rimraf = require('rimraf');
+var clean = require('./semantic/tasks/clean');
+var build = require('./semantic/tasks/build');
+var rmfile = require('./clean.js');
 
-gulp.task('build', 'Builds all files from source', build);
+gulp.task('clean', 'Clean dist folder', clean);
+gulp.task('build', 'Builds all files from source', ['clean'], build);
+gulp.task('rmfile', "Remove all files which not minified", ['build'], rmfile);
 
-gulp.task('default', ['build'],function(){
-	process.chdir('./public/semantic');
-	rimraf('components', function(err){
-		if (err) {
-			console.log(err);
-		}
-	});
-	rimraf('themes/basic', function(err){
-		if (err) {
-			console.log(err);
-		}
-	});
-	rimraf('themes/github', function(err){
-		if (err) {
-			console.log(err);
-		}
-	});
-	rimraf('semantic.css', function(err){
-		if (err) {
-			console.log(err);
-		}
-	});
-	rimraf('semantic.js', function(err){
-		if (err) {
-			console.log(err);	
-		}
-	});
+gulp.task('default', ['rmfile'], function () {
+    console.log('All done.')
 });
