914c661e0f94b6c59cc50afde6a30993daed4819
Vongola <vongola12324@coder.tw>
2016-08-17 22:30:53 +0800

    Add assignment table

--- database/migrations/2016_08_17_212917_create_issues_table.php
+++ database/migrations/2016_08_17_212917_create_issues_table.php
@@ -21,6 +21,17 @@ class CreateIssuesTable extends Migration
 
             $table->foreign('status_id')->references('id')->on('statuses')->onUpdate('cascade')->onDelete('cascade');
         });
+
+        // Create table for associating issues to users (Many-to-Many)
+        Schema::create('issue_user', function (Blueprint $table) {
+            $table->integer('issue_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('issue_id')->references('id')->on('issues')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->primary(['issue_id', 'user_id']);
+        });
     }
 
     /**
@@ -30,6 +41,7 @@ class CreateIssuesTable extends Migration
      */
     public function down()
     {
+        Schema::drop('issue_user');
         Schema::drop('issues');
     }
 }
